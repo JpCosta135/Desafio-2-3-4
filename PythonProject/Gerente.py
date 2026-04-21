0bebcc2d4bf9c6810fb0f81ce78290747cdf9efd
JpCosta135 <130411184+JpCosta135@users.noreply.github.com>
2026-04-21 19:11:43 -0300

    Refactor Gerente class by removing constructor

--- PythonProject/Gerente.py
+++ PythonProject/Gerente.py
@@ -4,8 +4,6 @@ from Funcionario import Funcionario
 
 
 class Gerente(Funcionario):
-    def __init__(self, nome, data_contratacao, salario):
-        super().__init__(nome, data_contratacao, salario)
 
     def aumentar_salario(self, salario, percentual):
         self.salario += salario * (percentual + (datetime.datetime.now().year - self.data_contratacao)/100)
